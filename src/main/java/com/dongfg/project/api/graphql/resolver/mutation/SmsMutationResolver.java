3411d3556550def00d0dbdfed0cd95bd966f4f90
dongfg <mail@dongfg.com>
2017-09-29 14:23:35 +0800

    add ratelimiter and token validate

--- src/main/java/com/dongfg/project/api/graphql/resolver/mutation/SmsMutationResolver.java
+++ src/main/java/com/dongfg/project/api/graphql/resolver/mutation/SmsMutationResolver.java
@@ -2,23 +2,31 @@ package com.dongfg.project.api.graphql.resolver.mutation;
 
 import com.coxautodev.graphql.tools.GraphQLMutationResolver;
 import com.dongfg.project.api.graphql.type.Sms;
-import com.dongfg.project.api.repository.SmsRepository;
+import com.dongfg.project.api.service.CommonService;
+import com.dongfg.project.api.service.SmsService;
+import com.dongfg.project.api.util.RamRateLimiter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import java.util.Date;
 
 @Component
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class SmsMutationResolver implements GraphQLMutationResolver {
 
     @NonNull
-    private SmsRepository smsRepository;
+    private SmsService smsService;
+
+    @NonNull
+    private CommonService commonService;
 
     public Sms sendSms(String token, Sms input) {
-        input.setCreateTime(new Date());
-        return smsRepository.save(input);
+        if (!RamRateLimiter.acquire(RamRateLimiter.LIMIT_KEY_SMS)) {
+            throw new RuntimeException("access rate limit exceeded");
+        }
+        if (!commonService.validateToken(token)) {
+            throw new RuntimeException("invalid token");
+        }
+        return smsService.sendSms(input);
     }
 }
