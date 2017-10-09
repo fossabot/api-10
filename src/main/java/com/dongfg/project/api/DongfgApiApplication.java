7e19bbe8738750a50245453b07cd2e7cbea462c5
dongfg <mail@dongfg.com>
2017-10-09 14:47:36 +0800

    enable search result memory cache

--- src/main/java/com/dongfg/project/api/DongfgApiApplication.java
+++ src/main/java/com/dongfg/project/api/DongfgApiApplication.java
@@ -2,10 +2,12 @@ package com.dongfg.project.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableCaching
 public class DongfgApiApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dongfg/project/api/service/BtService.java
+++ src/main/java/com/dongfg/project/api/service/BtService.java
@@ -7,6 +7,7 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -24,11 +25,8 @@ import java.util.stream.Collectors;
 @Slf4j
 public class BtService {
 
-    public List<BtInfo> btSearch(List<String> keyWords) {
-        return keyWords.parallelStream().map(this::btSearch).collect(Collectors.toList());
-    }
-
-    private BtInfo btSearch(String keyWords) {
+    @Cacheable("btSearch")
+    public BtInfo btSearch(String keyWords) {
         BtInfo btInfo = null;
 
 
