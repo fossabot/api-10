8073120a67d37f5db2ad2c7bcb7c14aff820343c
dongfg <mail@dongfg.com>
2017-11-09 14:52:36 +0800

    otp 生成及验证

---
+++ src/main/java/com/dongfg/project/api/graphql/resolver/mutation/TotpMutationResolver.java
@@ -0,0 +1,32 @@
+package com.dongfg.project.api.graphql.resolver.mutation;
+
+import com.coxautodev.graphql.tools.GraphQLMutationResolver;
+import com.dongfg.project.api.graphql.type.SecretKey;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author dongfg
+ * @date 17-11-9
+ */
+@Component
+@Slf4j
+public class TotpMutationResolver implements GraphQLMutationResolver {
+
+    private static GoogleAuthenticator googleAuthenticator = new GoogleAuthenticator();
+
+    private static final String OTP_AUTH_FORMAT = "otpauth://totp/%s:@%s?secret=%s";
+
+    public SecretKey createSecretKey(String site, String username) {
+        String secret = googleAuthenticator.createCredentials().getKey();
+        String otpAuthString = String.format(OTP_AUTH_FORMAT, site,username, secret);
+        return SecretKey.builder()
+                .secret(secret)
+                .qrCodeUrl(String.format("http://qr.liantu.com/api.php?w=400&text=%s", otpAuthString)).build();
+    }
+
+    public boolean validateCode(String secretKey, int code) {
+        return googleAuthenticator.authorize(secretKey, code);
+    }
+}
