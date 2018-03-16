6bc1b0d7334e5f427fbf17d05fdfe2e84a93b881
dongfg <mail@dongfg.com>
2018-03-16 09:38:28 +0800

    re init with gradle

---
+++ src/main/kotlin/com/dongfg/project/api/DongfgApiApplication.kt
@@ -0,0 +1,11 @@
+package com.dongfg.project.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DongfgApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<DongfgApiApplication>(*args)
+}
