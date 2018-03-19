0a33f5bb7947bd6b1610bacd598c7eaa8b486c70
dongfg <mail@dongfg.com>
2018-03-19 15:36:57 +0800

    remove unused import

--- src/main/kotlin/com/dongfg/project/api/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/dongfg/project/api/common/config/WebMvcConfig.kt
@@ -1,7 +1,6 @@
 package com.dongfg.project.api.common.config
 
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
