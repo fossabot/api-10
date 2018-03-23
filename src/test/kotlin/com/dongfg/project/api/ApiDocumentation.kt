5a94d87594fa33b9bc5a09c592d5fd2c03574113
dongfg <mail@dongfg.com>
2018-03-23 15:09:01 +0800

    restdoc

--- src/test/kotlin/com/dongfg/project/api/ApiDocumentation.kt
+++ src/test/kotlin/com/dongfg/project/api/ApiDocumentation.kt
@@ -1,14 +1,21 @@
 package com.dongfg.project.api
 
+import com.dongfg.project.api.common.Constants
+import com.dongfg.project.api.component.Totp
+import com.dongfg.project.api.model.Comment
 import com.dongfg.project.api.repository.CommentRepository
 import com.fasterxml.jackson.databind.ObjectMapper
 import mu.KLogging
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.http.MediaType
 import org.springframework.restdocs.JUnitRestDocumentation
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
@@ -19,6 +26,7 @@ import org.springframework.restdocs.payload.PayloadDocumentation.*
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit4.SpringRunner
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.DefaultMockMvcBuilder
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
@@ -31,6 +39,7 @@ import org.springframework.web.context.WebApplicationContext
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @ActiveProfiles("dev")
+@FixMethodOrder(MethodSorters.JVM)
 class ApiDocumentation {
 
     companion object : KLogging()
@@ -47,6 +56,9 @@ class ApiDocumentation {
     @Autowired
     private lateinit var objectMapper: ObjectMapper
 
+    @MockBean
+    private lateinit var totpMock: Totp
+
     private var mockMvc: MockMvc? = null
 
     @Before
@@ -54,22 +66,6 @@ class ApiDocumentation {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context!!)
                 .apply<DefaultMockMvcBuilder>(documentationConfiguration(this.restDocumentation))
                 .build()
-    }
-
-    @Test
-    @Throws(Exception::class)
-    fun comments() {
-        this.mockMvc?.perform(get("/comment")
-                .accept(MediaType.APPLICATION_JSON_UTF8))
-                ?.andExpect(status().isOk)
-                ?.andDo(document("comments",
-                        responseFields(
-                                fieldWithPath("[].comment").description("评论"),
-                                fieldWithPath("[].name").description("昵称"),
-                                fieldWithPath("[].email").optional().description("邮箱地址"),
-                                fieldWithPath("[].createTime").description("创建时间")
-                        )
-                ))
     }
 
     @Test
@@ -92,11 +88,62 @@ class ApiDocumentation {
                                 fieldWithPath("name").description("昵称"),
                                 fieldWithPath("email").description("邮箱地址")),
                         responseFields(
-                                fieldWithPath("comment").description("评论"),
-                                fieldWithPath("name").description("昵称"),
-                                fieldWithPath("email").description("邮箱地址"),
-                                fieldWithPath("createTime").description("创建时间")
+                                fieldWithPath("success").description("成功失败标志"),
+                                fieldWithPath("msg").description("返回消息")
                         )
                 ))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun comments() {
+        this.mockMvc?.perform(get("/comment")
+                .accept(MediaType.APPLICATION_JSON_UTF8))
+                ?.andExpect(status().isOk)
+                ?.andDo(document("comments",
+                        responseFields(
+                                fieldWithPath("[].comment").description("评论"),
+                                fieldWithPath("[].name").description("昵称"),
+                                fieldWithPath("[].email").optional().description("邮箱地址"),
+                                fieldWithPath("[].createTime").description("创建时间")
+                        )
+                ))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun sendMessage() {
+        val input = HashMap<String, String>(3)
+        input["type"] = Constants.MessageType.EMAIL.name
+        input["level"] = Constants.MessageLevel.ERROR.name
+        input["receiver"] = "dongfg@webmaster"
+        input["title"] = "restdoc"
+        input["content"] = "send from dongfg-api"
+        input["catalog"] = "test"
+        println(objectMapper.writeValueAsString(input))
+
+        Mockito.`when`(totpMock.validate("123456")).thenReturn(true)
+
+        this.mockMvc?.perform(post("/message")
+                .contentType(MediaType.APPLICATION_JSON_UTF8)
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .header("totp-auth-token", "123456")
+                .content(objectMapper.writeValueAsString(input)))
+                ?.andDo(MockMvcResultHandlers.print())
+                ?.andExpect(status().isOk)
+                ?.andDo(document("sendMessage",
+                        requestFields(
+                                fieldWithPath("type").description(Constants.MessageType.values()),
+                                fieldWithPath("level").description(Constants.MessageLevel.values()),
+                                fieldWithPath("receiver").description("接受者"),
+                                fieldWithPath("title").description("标题"),
+                                fieldWithPath("content").description("内容"),
+                                fieldWithPath("catalog").description("分类")),
+                        responseFields(
+                                fieldWithPath("success").description("成功失败标志"),
+                                fieldWithPath("msg").description("返回消息")
+                        )
+                ))
+
     }
 }
