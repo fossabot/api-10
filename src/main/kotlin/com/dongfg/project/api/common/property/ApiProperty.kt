88052387a0382efc3e479a49668429af8fbb1daa
dongfg <mail@dongfg.com>
2018-08-27 09:37:20 +0800

    remove unused property

--- src/main/kotlin/com/dongfg/project/api/common/property/ApiProperty.kt
+++ src/main/kotlin/com/dongfg/project/api/common/property/ApiProperty.kt
@@ -1,16 +1,12 @@
 package com.dongfg.project.api.common.property
 
 import org.springframework.boot.context.properties.ConfigurationProperties
-import org.springframework.boot.convert.DurationUnit
 import org.springframework.context.annotation.Configuration
-import java.time.Duration
-import java.time.temporal.ChronoUnit
 
 
 @Configuration
 @ConfigurationProperties("api")
 class ApiProperty {
-
     lateinit var version: String
 
     lateinit var url: String
@@ -21,42 +17,18 @@ class ApiProperty {
 
     var totp = Totp()
 
-    var push = Push()
-
-    var jwt = Jwt()
-
     var rss = Rss()
 
     inner class Admin {
-
         lateinit var url: String
     }
 
     inner class Sms {
-
         lateinit var secret: String
     }
 
     inner class Totp {
-
         lateinit var secret: String
-    }
-
-    inner class Push {
-
-        lateinit var mobile: String
-
-        lateinit var email: String
-
-        lateinit var openId: String
-    }
-
-    inner class Jwt {
-
-        lateinit var secret: String
-
-        @DurationUnit(ChronoUnit.MINUTES)
-        var timeout: Duration = Duration.ofHours(2)
     }
 
     inner class Rss {
