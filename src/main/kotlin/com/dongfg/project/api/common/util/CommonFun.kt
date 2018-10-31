c99678d1c6ea444c29544296974b6608692ad276
dongfg <mail@dongfg.com>
2018-10-31 15:50:20 +0800

    date formatter

--- src/main/kotlin/com/dongfg/project/api/common/util/CommonFun.kt
+++ src/main/kotlin/com/dongfg/project/api/common/util/CommonFun.kt
@@ -1,6 +1,8 @@
 package com.dongfg.project.api.common.util
 
 import org.json.JSONObject
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 inline fun <T : Any, R> whenNotNull(input: T?, callback: (T) -> R): R? {
@@ -15,4 +17,8 @@ class Json() : JSONObject() {
     infix fun <T> String.to(value: T) {
         put(this, value)
     }
+}
+
+fun LocalDateTime.toFormatString(): String {
+    return this.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
 }
